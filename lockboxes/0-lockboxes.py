4e7bbd2cba50fac9bbc8fee841fb264099e3f746
Fidan <aslanovafidan299@gmail.com>
2026-05-26 11:24:38 +0000

    My task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,26 +2,26 @@
 """
 Module for lockboxes problem
 """
- 
- 
+
+
 def canUnlockAll(boxes):
     """
     Determines if all locked boxes can be opened.
- 
+
     Args:
         boxes (list of lists): Each index is a box containing keys
- 
+
     Returns:
         bool: True if all boxes can be opened, False otherwise
     """
     n = len(boxes)
-    visited = set([0])       # Box 0 is already unlocked
-    keys = list(boxes[0])    # Start with keys found in box 0
- 
+    visited = set([0])
+    keys = list(boxes[0])
+
     while keys:
         key = keys.pop()
         if key < n and key not in visited:
             visited.add(key)
-            keys.extend(boxes[key])   # Add new keys found in this box
- 
+            keys.extend(boxes[key])
+
     return len(visited) == n
